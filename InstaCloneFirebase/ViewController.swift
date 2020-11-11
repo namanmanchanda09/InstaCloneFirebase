4ed4068ecb0970db82307f984ab63dbd3204d612
Naman Manchanda <namanmanchanda@Namans-MacBook-Air.local>
2020-11-11 18:21:02 +0530

    Project setup

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Naman Manchanda on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
