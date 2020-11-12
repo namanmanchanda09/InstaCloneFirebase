c4adca6450d8209ea6b2f1087dbc5fdd01034857
Naman Manchanda <namanmanchanda@Namans-MacBook-Air.local>
2020-11-12 11:52:41 +0530

    Sign Up added

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet var emailText: UITextField!
@@ -21,9 +22,36 @@ class ViewController: UIViewController {
     
     
     @IBAction func signUpClicked(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != ""{
+            
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil{
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        }else{
+            makeAlert(titleInput: "Error", messageInput: "Username/Password missing")
+
+        }
+
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
 }
 
+
+
  
 
+ 
